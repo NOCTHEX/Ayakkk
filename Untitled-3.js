e4a97e2bfb21691b9b2a8733188cb88a86ef775d
NOCTHEX <153111615+NOCTHEX@users.noreply.github.com>
2023-12-07 01:54:45 +0700

    Add files via upload

--- Untitled-3.js
+++ Untitled-3.js
@@ -5,6 +5,12 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
+    questtion.innerHTML = "Can we met after this?";
+    gif.src = 
+    "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
+});
+
+noBtn.addEventListener("mouseover", () => {
     const noBtnRect = noBtn.getBoundingClientRect();
     const maxX = window.innerWidth - noBtnRect.width;
     const maxY = window.innerHeight - noBtnRect.height;
